daae399bd75a2a4bd97ac0888d5b2f47a0af5671
jaemin.kim <jaemin.kim@shinzigames.com>
2026-09-16 23:34:08 +0900

    feat : 인벤토리 UI 세팅

--- Assets/06_Scripts/UI/HUDUI.cs
+++ Assets/06_Scripts/UI/HUDUI.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class UIinventory : MonoBehaviour
+public class HUDUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
